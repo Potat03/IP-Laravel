d85f75661c1e308759bf6c542619ef802ea61da6
Potat03 <nicholasyap2003@gmail.com>
2024-07-28 19:22:58 +0800

    test

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php 
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        // $validator = Validator::make($request->all(), [
+        //     'email' => 'required|email',
+        //     'password' => 'required',
+        // ]);
+        // if ($validator->fails()) {
+        //     return response()->json(['error' => $validator->errors()], 401);
+        // }
+        // if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+        //     $user = Auth::user();
+        //     $token = $user->createToken('MyApp')->accessToken;
+        //     return response()->json(['token' => $token], 200);
+        // } else {
+        //     return response()->json(['error' => 'Unauthorised'], 401);
+        // }
+        //redirect
+        return response()->json(['error' => 'Unauthorised'], 401);
+    }
+}
