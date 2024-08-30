e3e9bde584e0a1a6a2de6ac3dbe7946d5b08f537
HuskyBoiz <alvinloh03@gmail.com>
2024-08-30 14:07:36 +0800

    Another try

---
+++ resources/views/customerChat.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CustomerChatDemo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link href="{{ asset('css/support_popup.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div class="popup_btn">
+        <div class="popup_btn_txt">Need Some Help ? <strong>Click Here</strong></div>
+        <div class="popup_btn_icon"></div>
+    </div>
+
+
+    <div class="popup_box">
+        
+    </div>
+
+    <script>
+        
+    </script>
+</body>
+
+</html>
