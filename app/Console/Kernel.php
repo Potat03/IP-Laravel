086d96207ba33c3fb0cc99f09544f9f58182ab65
limwengni <lim1227december@gmail.com>
2024-09-20 22:55:13 +0800

    done setting up the api

---
+++ app/Console/Kernel.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ *
+ * Author: Lim Weng Ni
+ * Date: 20/09/2024
+ */
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        $schedule->command('check:lowstock')->twiceDaily(6, 18);
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+
+    protected $commands = [
+        \App\Console\Commands\CheckLowStockAndReorder::class,
+    ];
+}
